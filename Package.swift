aae0243796100ea6d33102ae25965e80126b9629
Sebastian Boettcher <jegge@gmx.net>
2023-09-03 20:34:42 +0200

    Removed dependency to swiftlint due to ci complications

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "OldMoofKit",
     platforms: [
-        .macOS(.v12), .iOS(.v14)
+        .macOS(.v12), .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -17,15 +17,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint", branch: "main")
+        // .package(url: "https://github.com/realm/SwiftLint", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OldMoofKit",
-            dependencies: [],
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+            dependencies: []
+            // plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
         ),
     ]
 )
