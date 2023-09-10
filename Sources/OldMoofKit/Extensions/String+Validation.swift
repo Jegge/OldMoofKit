1ad76d3cf2ed2012b3563fa0537b17ee398393e2
Sebastian Boettcher <jegge@gmx.net>
2023-09-10 21:56:14 +0200

    Added more tests

--- Sources/OldMoofKit/Extensions/String+Validation.swift
+++ Sources/OldMoofKit/Extensions/String+Validation.swift
@@ -28,4 +28,3 @@ extension String {
         }
     }
 }
-
--- Tests/OldMoofKitTests/TestBike.swift
+++ Tests/OldMoofKitTests/TestBike.swift
@@ -13,7 +13,7 @@ import Combine
 
 final class TestBike: XCTestCase {
 
-    private class SmartBike2018BluetoothConnectionMock: BluetoothConnectionProtocol, BluetoothScannerProtocol {
+    private class BluetoothConnectionMock: BluetoothConnectionProtocol, BluetoothScannerProtocol {
         static let macAddress = "1a:2b:3c:4d:5e:6f"
         static let encryptionKey = "4142434445464748494a4b4c4d4e4f50" // "ABCDEFGHIJKLMNOP"
         static let name = "MyBikeName"
@@ -36,7 +36,11 @@ final class TestBike: XCTestCase {
         var errorPublisher = PassthroughSubject<Error, Never>()
         var statePublisher = PassthroughSubject<BluetoothState, Never>()
 
-        var identifier: UUID = UUID(uuidString: "5d98872f-ab56-4b2c-96b5-254d45202857")!
+        init (identifier: UUID) {
+            self.identifier = identifier
+        }
+
+        var identifier: UUID
         var reconnectInterval: TimeInterval = 0.0
         var state: BluetoothState = .disconnected
 
@@ -66,8 +70,8 @@ final class TestBike: XCTestCase {
         }
     }
 
-    func testBikeConnection() async throws {
-        let bluetooth = SmartBike2018BluetoothConnectionMock()
+    func testBikeSmartBike2018Connection() async throws {
+        let bluetooth = BluetoothConnectionMock(identifier: UUID(uuidString: "5d98872f-ab56-4b2c-96b5-254d45202857")!)
         var authenticated = 0
         var setLight = false
         var setLock = false
@@ -76,7 +80,7 @@ final class TestBike: XCTestCase {
         var setMutedSounds = false
 
         bluetooth.handleWriteValue = { data, uuid, notifications in
-            let payload = Data(try! data.decrypt_aes_ecb_zero(key: Data(hexString: SmartBike2018BluetoothConnectionMock.encryptionKey)!).dropFirst(2))
+            let payload = Data(try! data.decrypt_aes_ecb_zero(key: Data(hexString: BluetoothConnectionMock.encryptionKey)!).dropFirst(2))
             switch uuid.uuidString {
             case "6ACB5523-E631-4069-944D-B8CA7598AD50": // write passcode
                 XCTAssertEqual("1dcb5f2321fe1ee12a616ad62c6bdde2", data.hexString, "Bike authentication failed")
@@ -108,7 +112,7 @@ final class TestBike: XCTestCase {
                 setMutedSounds = true 
 
             default:
-                break
+                XCTFail("Invalid write \(uuid.uuidString)")
             }
         }
 
@@ -130,7 +134,6 @@ final class TestBike: XCTestCase {
                 return Data([0x23, 0x42])
             case "6ACB5505-E631-4069-944D-B8CA7598AD50": // read muted sounds
                 return Data([0x00, 0x00, 0x00, 0x00])
-
             default:
                 return nil
             }
@@ -141,17 +144,18 @@ final class TestBike: XCTestCase {
         }
 
         let details = try BikeDetails(bleProfile: .smartBike2018,
-                                      macAddress: SmartBike2018BluetoothConnectionMock.macAddress,
-                                      encryptionKey: SmartBike2018BluetoothConnectionMock.encryptionKey,
-                                      name: SmartBike2018BluetoothConnectionMock.name,
-                                      frameNumber: SmartBike2018BluetoothConnectionMock.frameNumber,
-                                      modelName: SmartBike2018BluetoothConnectionMock.modelName,
-                                      smartModuleVersion: SmartBike2018BluetoothConnectionMock.smartModuleVersion)
+                                      macAddress: BluetoothConnectionMock.macAddress,
+                                      encryptionKey: BluetoothConnectionMock.encryptionKey,
+                                      name: BluetoothConnectionMock.name,
+                                      frameNumber: BluetoothConnectionMock.frameNumber,
+                                      modelName: BluetoothConnectionMock.modelName,
+                                      smartModuleVersion: BluetoothConnectionMock.smartModuleVersion)
 
         let bike = try await Bike(scanner: bluetooth, details: details, profile: details.profile!)
+        let rssi = await bike.signalStrength
 
         XCTAssertEqual(bike.details, details)
-
+        XCTAssertEqual(rssi, 42)
         let bikeErrors = bike.errorPublisher.sink { error in
             XCTFail("Bluetooth error: \(error)")
         }
@@ -188,6 +192,170 @@ final class TestBike: XCTestCase {
         try await bike.set(mutedSounds: [.lockState, .moduleState])
         while !setMutedSounds { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
         XCTAssertEqual(bike.mutedSounds, [.lockState, .moduleState])
+
+        bike.disconnect()
+        XCTAssertEqual(bike.state, .disconnected)
+
+        bluetoothErrors.cancel()
+        bikeErrors.cancel()
+    }
+
+    func testBikeElectrified2018Connection() async throws {
+        let bluetooth = BluetoothConnectionMock(identifier: UUID(uuidString: "5d98872f-ab56-4b2c-96b5-254d45202857")!)
+        var authenticated = 0
+        var setModuleState = false
+        var setLight = false
+        var setLock = false
+        var setMutedSounds = false
+        var setUnit = false
+        var setMotorAssistance = 0
+
+        var moduleState = ModuleState.standby
+        var lighting = Lighting.off
+        var unit = Unit.imperial
+        var motorAssistance = MotorAssistance.one
+        var region = Region.eu
+        var mutedSounds = MutedSounds.none
+        var lock = Lock.locked
+
+        func makeParameters() -> Data {
+            return Data([0x00,
+                         0x00,
+                         (moduleState == .on ? 0x01 : 0x00) + UInt8(Alarm.automatic.rawValue << 1),
+                         lock == .locked ? 0x02 : 0x00,
+                         0x23, // speed
+                         0x42, // motor battery level
+                         0x47, // module battery level
+                         lighting.rawValue + UInt8(unit.rawValue << 2),
+                         UInt8(motorAssistance.rawValue << 2) + region.rawValue,
+                         0x00,
+                         UInt8(mutedSounds.rawValue >> 6),
+                         12,34,56,78, // 131230158.0 km
+                         UInt8(ErrorCode.gsmRegistrationFailure.data.first! << 3) + 0x01]) // charging
+        }
+
+        bluetooth.handleWriteValue = { data, uuid, notifications in
+            let payload = Data(try! data.decrypt_aes_ecb_zero(key: Data(hexString: BluetoothConnectionMock.encryptionKey)!).dropFirst(2))
+            let command = payload.first!
+
+            XCTAssertEqual(uuid.uuidString, "8E7F1A53-087A-44C9-B292-A2C628FDD9AA")
+
+            switch command {
+            case Electified2018Profile.Command.setPasscode:
+                XCTAssertTrue(BluetoothConnectionMock.encryptionKey.hasPrefix(payload[1...12].hexString), "Bike authentication failed")
+                authenticated += 1
+                return
+
+            case Electified2018Profile.Command.setModuleState:
+                moduleState = ModuleState(rawValue: payload[1])!
+                setModuleState = true
+
+            case Electified2018Profile.Command.setLightning:
+                lighting = Lighting(rawValue: payload[1])!
+                setLight = true
+
+            case Electified2018Profile.Command.requestLock:
+                lock = Lock.unlocked
+                setLock = true
+
+            case Electified2018Profile.Command.setSound:
+                mutedSounds =  MutedSounds(rawValue: UInt16(payload[2]) << 6)
+                setMutedSounds = true
+
+            case Electified2018Profile.Command.setMotorAssistance:
+                motorAssistance = MotorAssistance(rawValue: payload[1])!
+                region = Region(rawValue: payload[2])!
+                setMotorAssistance += 1
+
+            case Electified2018Profile.Command.setUnit:
+                unit = Unit(rawValue: payload[1])!
+                setUnit = true
+
+            default:
+                return
+            }
+
+            let parameters = makeParameters()
+            let result: Data = (try? parameters.encrypt_aes_ecb_zero(key: Data(hexString: BluetoothConnectionMock.encryptionKey)!)) ?? Data()
+            let notification = BluetoothNotification(uuid: CBUUID(string: "8E7F1A54-087A-44C9-B292-A2C628FDD9AA"), data: result)
+            notifications.send(notification)
+        }
+
+        bluetooth.handleReadValue = { uuid in
+            switch uuid.uuidString {
+            case "8E7F1A51-087A-44C9-B292-A2C628FDD9AA": // read challenge
+                return Data([0x23, 0x42])
+            case "8E7F1A54-087A-44C9-B292-A2C628FDD9AA": // read parameters
+                let parameters = makeParameters()
+                return try? parameters.encrypt_aes_ecb_zero(key: Data(hexString: BluetoothConnectionMock.encryptionKey)!)
+            default:
+                XCTFail("Invalid read \(uuid.uuidString)")
+                return nil
+            }
+        }
+
+        let bluetoothErrors = bluetooth.errorPublisher.sink { error in
+            XCTFail("Bluetooth error: \(error)")
+        }
+
+        let details = try BikeDetails(bleProfile: .electrified2018,
+                                      macAddress: BluetoothConnectionMock.macAddress,
+                                      encryptionKey: BluetoothConnectionMock.encryptionKey,
+                                      name: BluetoothConnectionMock.name,
+                                      frameNumber: BluetoothConnectionMock.frameNumber,
+                                      modelName: BluetoothConnectionMock.modelName,
+                                      smartModuleVersion: BluetoothConnectionMock.smartModuleVersion)
+
+        let bike = try await Bike(scanner: bluetooth, details: details, profile: details.profile!)
+        let rssi = await bike.signalStrength
+
+        XCTAssertEqual(bike.details, details)
+        XCTAssertEqual(rssi, 42)
+        let bikeErrors = bike.errorPublisher.sink { error in
+            XCTFail("Bluetooth error: \(error)")
+        }
+
+        XCTAssertEqual(bike.state, .disconnected)
+        try await bike.connect()
+        XCTAssertEqual(bike.state, .connected)
+        XCTAssertEqual(authenticated, 1)
+
+        XCTAssertEqual(bike.distance, 131230158.0)
+        XCTAssertEqual(bike.moduleState, .standby)
+        XCTAssertEqual(bike.errorCode, ErrorCode.gsmRegistrationFailure)
+        XCTAssertEqual(bike.lighting, .off)
+        XCTAssertEqual(bike.lock, .locked)
+        XCTAssertEqual(bike.alarm, .automatic)
+        XCTAssertEqual(bike.mutedSounds, .none)
+        XCTAssertEqual(bike.mutedSounds, .none)
+
+        try await bike.wakeup()
+        while !setModuleState { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) } // this is a dirty hack. do not use this in production code!
+        XCTAssertEqual(bike.moduleState, .on)
+
+        try await bike.set(lighting: .alwaysOn)
+        while !setLight { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
+        XCTAssertEqual(bike.lighting, .alwaysOn)
+
+        try await bike.set(lock: .unlocked)
+        while !setLock { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
+        XCTAssertEqual(bike.lock, .unlocked)
+
+        try await bike.set(mutedSounds: [.lockState, .moduleState])
+        while !setMutedSounds { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
+        XCTAssertEqual(bike.mutedSounds, [.lockState, .moduleState])
+
+        try await bike.set(unit: Unit.metric)
+        while !setUnit { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
+        XCTAssertEqual(bike.unit, .metric)
+
+        try await bike.set(region: Region.japan)
+        while setMotorAssistance == 0 { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
+        XCTAssertEqual(bike.region, .japan)
+
+        try await bike.set(motorAssistance: .four)
+        while setMotorAssistance == 1 { try await Task.sleep(nanoseconds: NSEC_PER_SEC >> 2) }
+        XCTAssertEqual(bike.motorAssistance, .four)
 
         bike.disconnect()
         XCTAssertEqual(bike.state, .disconnected)
